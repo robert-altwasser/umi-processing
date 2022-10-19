0b8d26d77ffd8cebde9e637e0bae6b410665283f
Robert Altwasser <robert.altwasser@charite.de>
2022-10-19 15:34:57 +0200

    cleaned code

--- umi_all/snake.sh
+++ umi_all/snake.sh
@@ -16,12 +16,12 @@ snakemake \
     --jobs 60 \
     --cluster-config ~/work/umi-data-processing/config/cluster_config.yaml \
     --profile=cubi-v1 \
-    --restart-times 2 \
-    --keep-going \
+    --restart-times 0 \
     --rerun-incomplete \
     --use-conda --conda-prefix=/fast/users/altwassr_c/work/conda-envs/
 # --touch \
 # --skip-script-cleanup \
 # --reason \
+    #--keep-going \
 
 # --until annovar \
