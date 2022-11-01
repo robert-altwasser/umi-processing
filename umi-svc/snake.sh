d6f045d53944de1cb3be93c49baa71a6c7131f74
Robert Altwasser <robert.altwasser@charite.de>
2022-11-01 11:08:15 +0100

    delly=1.1.5 is necessary for the pipeline

--- umi-svc/snake.sh
+++ umi-svc/snake.sh
@@ -6,7 +6,7 @@ snakemake \
     --jobs 120 \
     --cluster-config ~/work/umi-data-processing/config/cluster_scv.yaml \
     --profile=cubi-v1 \
-    --restart-times 0 \
+    --restart-times 1 \
     --keep-going \
     --rerun-incomplete \
     --use-conda --conda-prefix=/fast/users/altwassr_c/work/conda-envs/
